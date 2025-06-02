7e3b45f1845fd9f020dafb20fb8665a0526a3566
imnb57 <imnitesh57@gmail.com>
2025-06-02 11:04:10 +0545

    commit

--- class/Hello.java
+++
@@ -1,6 +0,0 @@
-/** 1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
-public class Hello{
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
